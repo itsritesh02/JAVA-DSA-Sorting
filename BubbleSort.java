8c87074bbf097399e94c3c79fca3c3a4e4be8ba0
itsritesh02 <itsritesh02@gmail.com>
2026-06-09 12:27:27 +0530

    bubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,23 @@
 public class BubbleSort {
-  
+  static void bubble(int[] a) {
+    int n = a.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (a[j] > a[j + 1]) {
+          int temp = a[j];
+          a[j] = a[j + 1];
+          a[j+1] = temp;
+        }
+      }
+???
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] a = { 5, 9, 7, 6, 2 };
+    bubble(a);
+    for (int i : a) {
+      System.out.print(i + " ");
+    }
+  }
 }
