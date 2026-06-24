3cf418691a05748a431ba0418a3914bf49cf0280
itsritesh02 <itsritesh02@gmail.com>
2026-06-24 16:09:56 +0530

    mergeSort

---
+++ QuickSort.java
@@ -0,0 +1,8 @@
+public class QuickSort {
+  public static void main(String[] args) {
+    int [] arr ={6, 3, 1, 5, 4};
+    System.out.println("Array before Sorting");
+    System.out.println();
+
+  }
+}
